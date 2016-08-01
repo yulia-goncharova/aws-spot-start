a9429699fc9c4ca124794b81a03c44a8ac5d91cb
Yulia Goncharova <letoveter@gmail.com>
2016-08-01 18:05:56 +0300

    launch fixes

--- launch.py
+++ launch.py
@@ -28,7 +28,7 @@ def make_request(query, get_output=False, verbose=0):
 debug = 1
 
 spot_result = make_request('aws ec2 request-spot-instances --spot-price  {} --instance-count 1 --type one-time ' \
-               '--config-specification file://config/specification.json'.format(MAX_PRICE), True, debug)
+               '--launch-specification file://config/specification.json'.format(MAX_PRICE), True, debug)
 spot_id = spot_result['SpotInstanceRequests'][0]['SpotInstanceRequestId']
 
 make_request('aws ec2 wait spot-instance-request-fulfilled --spot-instance-request-ids {}'.format(spot_id), False, debug)
@@ -43,10 +43,10 @@ host_result = make_request('aws ec2 describe-instances --instance-ids  {}'.forma
 host = host_result['Reservations'][0]['Instances'][0]['PublicDnsName']
 
 make_request('ssh -o StrictHostKeyChecking=no ubuntu@{}'.format(host), False, debug)
-make_request('scp -i {} -r ../data ubuntu@{}:~'.format(PEM_FILE, host), False, debug)
-make_request('scp -i {} -r ../notepads ubuntu@{}:~'.format(PEM_FILE, host), False, debug)
+make_request('scp -i {} -r data ubuntu@{}:~'.format(PEM_FILE, host), False, debug)
+make_request('scp -i {} -r notepads ubuntu@{}:~'.format(PEM_FILE, host), False, debug)
 
-make_request("ssh -i {} ubuntu@{} 'bash -s' < remote_setup.sh".format(PEM_FILE, host), False, debug)
+make_request("ssh -i {} ubuntu@{} 'bash -s' < config/remote_setup.sh".format(PEM_FILE, host), False, debug)
 
 new = 2
 url = 'https://{}:8888'.format(host)
